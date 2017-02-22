2b2110effd0edd8871cbab3dbc68673950f05ec5
Mauricio Conde <condexinastle@gmail.com>
2017-02-22 16:23:43 -0600

    chore: Added convenience extensions for CALayer, UILabel, UItextView & UIViewController

--- CXFramework/Extension/CX+UIViewController.swift
+++ CXFramework/Extension/CX+UIViewController.swift
@@ -40,6 +40,38 @@ public extension UIViewController {
         lBarBtnItem.target = target != nil ? target : self
         lBarBtnItem.action = action
     }
+    
+    /// ---
+    /// Sets an image to the 'rightBarButtonItem' property of the navigation item and
+    /// configures it with the specified action and target
+    ///
+    /// - parameters:
+    ///     - imgName: The image name
+    ///     - target: The object that receives the action message.
+    ///     - action: The action to send to target when this item is selected.
+    public func configureImageForRightBarButtonItem(imgName: String,
+                                                   target: AnyObject?,
+                                                   action: Selector){
+        var icon: UIImageView!
+        
+        let image  = UIImage(named: imgName)?.withRenderingMode(UIImageRenderingMode.alwaysOriginal)
+        icon = UIImageView(image: image?.resize(width: 30, height: 30))
+        icon.alpha = 0.5
+        
+        guard self.navigationItem.leftBarButtonItems != nil else{
+            let rBarBtnItem = UIBarButtonItem(image: icon.image,
+                                              style: UIBarButtonItemStyle.plain,
+                                              target: self,
+                                              action: action)
+            self.navigationItem.leftBarButtonItem = rBarBtnItem
+            return
+        }
+        
+        let rBarBtnItem: UIBarButtonItem = self.navigationItem.leftBarButtonItems!.first!
+        rBarBtnItem.customView = icon
+        rBarBtnItem.target = target != nil ? target : self
+        rBarBtnItem.action = action
+    }
 }
 
 // MARK:- Alert related methods
