acbdcdbb287e49652c6a654f5c1a41578c8649b7
Mauricio Conde Xinastle <condexinastle@gmail.com>
2017-05-16 14:02:26 -0500

    refactor: Date,UITextfield & String extensions related

--- CXFramework/Extension/CX+String.swift
+++ CXFramework/Extension/CX+String.swift
@@ -6,4 +6,12 @@
 //  Copyright © 2017 Mauricio Conde Xinastle. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public extension String {
+    
+    /// Calculate the width of a text string of a specific font and font-size
+    public func getPreferredSize(withFont font: UIFont) -> CGSize {
+        return (self as NSString).size(attributes: [NSFontAttributeName: font])
+    }
+}
