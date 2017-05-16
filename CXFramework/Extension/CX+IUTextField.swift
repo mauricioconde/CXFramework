03971dd68512adc8223cc4c2b7eeb78e58dda56e
Mauricio Conde Xinastle <condexinastle@gmail.com>
2017-05-16 14:01:09 -0500

    feat: Add convenience methods for UITextfield & String

---
+++ CXFramework/Extension/CX+IUTextField.swift
@@ -0,0 +1,9 @@
+//
+//  CX+IUTextField.swift
+//  CXFramework
+//
+//  Created by Mauricio Conde on 16/05/17.
+//  Copyright © 2017 Mauricio Conde Xinastle. All rights reserved.
+//
+
+import Foundation
---
+++ CXFramework/Extension/CX+String.swift
@@ -0,0 +1,9 @@
+//
+//  CX+String.swift
+//  CXFramework
+//
+//  Created by Mauricio Conde on 16/05/17.
+//  Copyright © 2017 Mauricio Conde Xinastle. All rights reserved.
+//
+
+import Foundation
